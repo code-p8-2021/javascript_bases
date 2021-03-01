89b781c86747345d853c355843b9ea21418ec347
Glitch (daily-mire-freon) <none>
2021-03-01 22:31:28 +0000

    🏹🍄 Checkpoint ./_00_Types/js/appplus.js:27209205/134 ./_00_Types/04_VariablesPlus.html:27209205/1828

--- _00_Types/js/appplus.js
+++ _00_Types/js/appplus.js
@@ -1,4 +1,7 @@
+// cf. 04_VariablesPlus.html
 
+
+// Les tableaux
 let eleves = ['Jean', 'Marc', 'Marion'];
 let demo = [true, 10, 'Marc'];
 console.log("eleves == "+ eleves);
@@ -19,9 +22,10 @@ console.log("eleve.nom == "+ eleve.nom); // Jean
 console.log("eleve.notes == "+ eleve.notes); // [10, 4, 18]
 console.log("eleve.notes[1] == "+ eleve.notes[1]); // 4
 // On peut aussi utiliser une notation proche de celle des tableaux
-console.log(eleve['notes']); // [10, 4, 18]
+console.log("eleve['notes'] == "+ eleve['notes']); // [10, 4, 18]
 
 console.warn("\n\n----\n\n");
+
 
 
 
@@ -34,5 +38,5 @@ let eleve_carnet = {
     }   
 }
 // Pour récupérer la note de math de l'élève on peut alors faire
-console.log(eleve_carnet.notes.math); // 18
-console.log(eleve_carnet.nom); // undefined
+console.log("eleve_carnet.notes.math == "+ eleve_carnet.notes.math); // 18
+console.log("eleve_carnet.nom == "+ eleve_carnet.nom); // undefined
