743bb3cd1d8455e27489cfee2ca9e4b2a3e8d42a
Glitch (daily-mire-freon) <none>
2021-03-01 17:37:09 +0000

    🎪🚇 Checkpoint ./_03_Boucles/js/01_for_loop.js:27209205/1563 ./_03_Boucles/01_for_loop.html:27209205/1062 ./css/style.css:27209205/77

--- _03_Boucles/js/01_for_loop.js
+++ _03_Boucles/js/01_for_loop.js
@@ -1,5 +1,34 @@
 for(let i = 0; i < 10; i++){
     document.getElementById('p1').innerHTML +=
-    '<i>i</i> stocke la valeur <b>' + i + ' lors du passage n°'
-    + (i + 1) + ' dans la boucle<br>';
+    '<i>i</i> stocke la valeur <b>' + i + '</b> lors du passage <u>n°'
+    + (i + 1) + '</u> dans la boucle<br>';
 }
+
+
+
+
+// -------
+
+
+// On peut ajouter des conditions dans la boucle
+// % c'est le modulo 
+// c'est l'écart qu'il reste entre le premier chiffre (i) et le second (2)
+// une fois que l'un divise l'autre...
+// 2/2 == 0 
+// 
+
+
+for(let i = 0; i <= 10; i++){
+    //Si i / 2 possède un reste, alors i est impair
+    if(i % 5 != 0){
+          document.getElementById('p2').innerHTML +=
+          '<strike><i>i</i> stocke la valeur <b>' + i + '</b> lors du passage <u>n°'
+          + (i + 1) + '</u> dans la boucle</strike><br>';
+        continue;
+    }
+    document.getElementById('p2').innerHTML +=
+    '<i>i</i> stocke la valeur <b>' + i + '</b> lors du passage <u>n°'
+    + (i + 1) + '</u> dans la boucle<br>';
+}
+
+
