5aca758d170520810a51c8df8a124dcbdc8c34e9
Glitch (daily-mire-freon) <none>
2021-03-01 17:49:09 +0000

    👮🛫 Checkpoint ./_03_Boucles/js/01_for_loop.js:27209205/629

--- _03_Boucles/js/01_for_loop.js
+++ _03_Boucles/js/01_for_loop.js
@@ -11,24 +11,25 @@ for(let i = 0; i < 10; i++){
 
 
 // On peut ajouter des conditions dans la boucle
-// % c'est le modulo 
-// c'est l'écart qu'il reste entre le premier chiffre (i) et le second (2)
-// une fois que l'un divise l'autre...
-// 2/2 == 0 
-// 
+// % est un modulo 
+// Il donne comme résultat 
+// le reste dans une division 
+// 2%2 == 0 
+// 4%5
 
 
 for(let i = 0; i <= 10; i++){
     //Si i / 2 possède un reste, alors i est impair
-    if(i % 5 != 0){
+  let modulo = 4;
+    if(i % modulo != 0){
           document.getElementById('p2').innerHTML +=
           '<strike><i>i</i> stocke la valeur <b>' + i + '</b> lors du passage <u>n°'
-          + (i + 1) + '</u> dans la boucle</strike><br>';
+          + (i + 1) + '</u> dans la boucle (('+  i % modulo  +'))</strike><br>';
         continue;
     }
     document.getElementById('p2').innerHTML +=
     '<i>i</i> stocke la valeur <b>' + i + '</b> lors du passage <u>n°'
-    + (i + 1) + '</u> dans la boucle<br>';
+    + (i + 1) + '</u> dans la boucle (('+  i % modulo  +'))<br>';
 }
 
 
