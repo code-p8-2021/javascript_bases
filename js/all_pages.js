8f3092d05c707e83f91c84b2b857b3d5f31ef379
Glitch (daily-mire-freon) <none>
2021-03-02 15:18:07 +0000

    😬🌍 Checkpoint ./js/all_pages.js:27209205/875 ./roadmap.html:27209205/4121 ./_00_Types/01_variables_let_types.html:27209205/210 ./_00_Types/02_variables_let_types.html:27209205/151 ./_00_Types/03_variablesPlus.html:27209205/159 ./_00_Types/04_list_childs.html:27209205/214 ./_01_Operators/00_Arithmetic.html:27209205/151 ./_01_Operators/00_Arithmetic_alt.html:27209205/151 ./_01_Operators/01_Comparison.html:27209205/151 ./_01_Operators/02_Logical.html:27209205/151 ./_01_Operators/03_Assignment.html:27209205/151 ./_01_Operators/04_Conditional.html:27209205/151 ./_01_Operators/05_Conditions_Types.html:27209205/151 ./_02_Conditions/00_if_else.html:27209205/151 ./_02_Conditions/01_switch_case.html:27209205/151 ./_03_Boucles/00_while.html:27209205/159 ./_03_Boucles/01_for_loop.html:27209205/159 ./_04_Functions/00_Scoping.html:27209205/159 ./_04_Functions/01_Scoping.html:27209205/159 ./_04_Functions/02_Hosting_error.html:27209205/160

--- js/all_pages.js
+++ js/all_pages.js
@@ -1,19 +1,7 @@
-// Immediately-invoked function expression
-(function() {
-    // Load the script
-    var script = document.createElement("SCRIPT");
-    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js';
-    script.type = 'text/javascript';
-    script.onload = function() {
-        var $ = window.jQuery;
-        // Use $ here...
-    };
-    document.getElementsByTagName("head")[0].appendChild(script);
-})();
     $(window).load(function(){
       $( 'a[href^="//"],a[href^="http"]' )
     .not( '[href*="' + window.location.hostname + '"]' ).click(function(){
-            alert("top")
+            alert("Pensez à ouvrir les liens hors de l'iframe de glitch.com\nMerci...")
             return true;
         })
     })
